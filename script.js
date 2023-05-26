de9b6eab72e3957d4c70d78d5edf510cedf77ffc
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-26 12:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const box = document.querySelector("#box");
+
+box.style.width = "100px"
+box.style.height = "100px"
+box.style.backgroundColor = "red"
+
+box.style.animation = "pulse 1s linear infinite";
+
+let style = document.createElement("style");
+
+style.innerHTML = `@keyframes pulse {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.2);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }`;
+
+document.body.append(style);
+
